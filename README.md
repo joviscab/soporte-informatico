821d1bdf0f88bf91ce667a05cc13a5132c38c0ea
Joviscab <152897880+joviscab@users.noreply.github.com>
2024-08-24 00:37:26 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Odin Project - Homepage
+
+## Who am I? What is this?
+
+Hello, my name is Joviscab and this is part of my learning to code journey!
+
+This project is called Homepage and is part of the Javascript Course of The Odin Project. The idea of this project is to put to practice all the responsiveness knowledge that I have acquired until now. In this project I am going to build a Homepage, following the instructions of Odin's lesson! The idea is to make the page responsive in 3 different sizes for the first time.
+
+## Skills
+
+With this project my idea is to check the responsive web design skills that I have learned until now, as well as using it as an exercise to remember all the lessons until this point.
+
+## If you are reading this
+
+Thank you for your interest! I'm still here, lit bit more of HTML and CSS, let's goooo!
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/js/main.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // Clean the output directory before emit.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[hash][ext][query]",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+    hot: true,
+    open: true,
+    client: {
+      overlay: false,
+    },
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
+  ],
+};
